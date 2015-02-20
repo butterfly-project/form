86047ce364292446eb7e34fb84140a55cf063a71
Fakhertdinov Marat <marat.fakhertdinov@gmail.com>
2015-02-20 09:45:00 +0600

    Add validator ArrayHasKey

---
+++ src/Validation/ArrayHasKey.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Butterfly\Component\Form\Validation;
+
+/**
+ * @author Marat Fakhertdinov <marat.fakhertdinov@gmail.com>
+ */
+class ArrayHasKey implements IValidator
+{
+    /**
+     * @var string
+     */
+    protected $key;
+
+    /**
+     * @param string $key
+     */
+    public function __construct($key)
+    {
+        $this->key = $key;
+    }
+
+    /**
+     * @param mixed $value
+     * @return bool
+     * @throws \InvalidArgumentException if incorrect value type
+     */
+    public function check($value)
+    {
+        if (!is_array($value)) {
+            throw new \InvalidArgumentException(sprintf("Expected array, given: %s", var_export($value, true)));
+        }
+
+        return array_key_exists($this->key, $value);
+    }
+}
---
+++ tests/unit/Validation/ArrayHasKeyTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Butterfly\Component\Form\Tests\Validation;
+
+use Butterfly\Component\Form\Validation\ArrayHasKey;
+
+/**
+ * @author Marat Fakhertdinov <marat.fakhertdinov@gmail.com>
+ */
+class ArrayHasKeyTest extends \PHPUnit_Framework_TestCase
+{
+    public function getDataForTestCheck()
+    {
+        return array(
+            array(array('foo' => 1), 'foo', true, 'check exists key'),
+            array(array('foo' => 1), 'bar', false, 'check not existing key'),
+            array(array('foo' => null), 'foo', true, 'check existing key with "null" value'),
+        );
+    }
+
+    /**
+     * @dataProvider getDataForTestCheck
+     *
+     * @param mixed $value
+     * @param string $key
+     * @param bool $expectedResult
+     * @param string $caseMessage
+     */
+    public function testCheck($value, $key, $expectedResult, $caseMessage)
+    {
+        $validator = new ArrayHasKey($key);
+
+        $this->assertEquals($expectedResult, $validator->check($value), $caseMessage);
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testCheckIfInvalidArgument()
+    {
+        $validator = new ArrayHasKey(1);
+
+        $validator->check('abc');
+    }
+}
